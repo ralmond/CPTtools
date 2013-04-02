4476633ca85572846b4cdf8a93605f8ab16ce096
ralmond <ralmond@fba4ab2f-d5a5-0410-83fd-9337b5567c92>
2013-04-02 02:16:22 +0000

    Refactor and start of docs for DPC model.

--- CPTtools/trunk/R/DGR.R
+++ CPTtools/trunk/R/DGR.R
@@ -10,73 +10,6 @@
 ### skillLevels and obsLevels are assumed to be ordered highest to
 ### lowest.
 
-### Unlike the DiBello-Samejima model, lnAlphas, betas, and
-### lnAlphas --- set of log slopes, one for each obs level -1
-### betas --- difficulty (-intercept) parameter
-### rules --- Function for computing effective theta.  This should have the
-### signature   function(thetas,alphas,beta)
-
-### The first one in the list representes the difference between the
-### highest and next lowest obs state, and so forth.
-calcDGRTable <- function (skillLevels, obsLevels, lnAlphas, betas, 
-                         rules="Compensatory") {
-  
-  k <- length(obsLevels)
-  if (!is.list(lnAlphas)) lnAlphas <- list(lnAlphas)
-  if (length(lnAlphas) != k-1) lnAlphas <- rep(lnAlphas,k-1)
-  if (!is.list(betas)) betas <- list(betas)
-  if (length(betas) != k-1) betas <- rep(betas,k-1)
-  if (!is.list(rules)) rules <- list(rules)
-  if (length(rules) != k-1) rules <- rep(rules,k-1)
-
-
-  pdims <- sapply(skillLevels,length)
-  tvals <- lapply(pdims,effectiveThetas)
-  thetas <- do.call("expand.grid",tvals)
-  
-  pt <- matrix(0,nrow(thetas),k-1)
-  for (kk in 1:(k-1)) {
-    etheta <- do.call(rules[[kk]],
-                      list(thetas,exp(lnAlphas[[kk]]),betas[[kk]]))
-    pt[,kk] <- 1/(1+exp(-1.7*etheta))
-  }
-  gradedResponse(pt,k,obsLevels)
-}
-
-gradedResponse <- function (pt,k,obsLevels=NULL) {
-  ## The cummax corrects for problems where the
-  ## Pr(X> k+1) is greater than Pr(X>k)
-  pt <- cbind(0,t(apply(pt,1,cummax)),1)
-  probs <- pt[,2:(k+1)]-pt[,1:k]
-  # probs <- ifelse(probs<0,0,probs)
-  if (!is.null(obsLevels)) {
-    dimnames(probs) <- list(NULL,obsLevels)
-  }
-  probs
-}
-
-
-calcDGRFrame <-
-function (skillLevels, obsLevels, lnAlphas, betas, 
-          rules="Compensatory") {
-  result <- data.frame(expand.grid(skillLevels),
-                       calcDGRTable(skillLevels,paste(obsLevels),
-                                   lnAlphas,betas,rules))
-  if (is.numeric(obsLevels) ||
-      names(result)[length(skillLevels)+1]!=paste(obsLevels[1])) {
-    ## R is "helpfully" fixing our numeric labels.  Need to insist.
-    names(result) <- c(names(skillLevels),paste(obsLevels))
-  }
-  result
-
-}
-
-
-### skillLevels --- list of labels for the skill level variable.
-### obsLevels --- labels for obserable values.
-### skillLevels and obsLevels are assumed to be ordered highest to
-### lowest.
-
 ### Unlike the DiBello-Samejima rule, lnAlphas, betas, and
 ### lnAlphas --- set of log slopes, one for each obs level
 ### beta --- difficulty (-intercept) parameter
@@ -86,7 +19,8 @@ function (skillLevels, obsLevels, lnAlphas, betas,
 ### The first one in the list representes the difference between the
 ### highest and next lowest obs state, and so forth.
 calcDPCTable <- function (skillLevels, obsLevels, lnAlphas, betas, 
-                         rules="Compensatory") {
+                         rules="Compensatory", link="partialCredit",
+                          linkScale=NULL) {
   
   k <- length(obsLevels)
   if (!is.list(lnAlphas)) lnAlphas <- list(lnAlphas)
@@ -105,10 +39,41 @@ calcDPCTable <- function (skillLevels, obsLevels, lnAlphas, betas,
     et[,kk] <- do.call(rules[[kk]],
                       list(thetas,exp(lnAlphas[[kk]]),betas[[kk]]))
   }
-  partialCredit(et,k,obsLevels)
+  do.call(link,list(et,linkScale,obsLevels))
 }
 
-partialCredit <- function (et,k,obsLevels=NULL) {
+calcDPCFrame <-
+function (skillLevels, obsLevels, alphas, betas, 
+          rules="Compensatory", link="partialCredit",
+          linkScale=NULL) {
+  result <- data.frame(expand.grid(skillLevels),
+                       calcDPCTable(skillLevels,paste(obsLevels),
+                                    alphas,betas,rules,link,
+                                    linkScale))
+  if (is.numeric(obsLevels) ||
+      names(result)[length(skillLevels)+1]!=paste(obsLevels[1])) {
+    ## R is "helpfully" fixing our numeric labels.  Need to insist.
+    names(result) <- c(names(skillLevels),paste(obsLevels))
+  }
+  result
+
+}
+
+
+### Link Functions
+### These are a function of three arguments
+### et -- a table of effective thetas, one for row for each
+###       configuration of parent variables, and one column for
+###       each child state except for the last.
+### k -- the number of states of the child variable.
+### linkScale --- a scale parameter used by some link functions.
+### obsLevel --- a list of names of the observables, assume to be
+### sorted from highest to lowest.
+
+### It returns a conditional probility table.
+
+partialCredit <- function (et,linkScale=NULL,obsLevels=NULL) {
+  k <- nrow(et)+1
   zt <- t(apply(et,1,function(x) rev(cumsum(rev(x)))))
   pt <- cbind(exp(1.7*zt),1)
   pt <- sweep(pt,1,apply(pt,1,sum),"/")
@@ -120,17 +85,19 @@ partialCredit <- function (et,k,obsLevels=NULL) {
   probs
 }
 
-calcDPCFrame <-
-function (skillLevels, obsLevels, alphas, betas, 
-          rules="Compensatory") {
-  result <- data.frame(expand.grid(skillLevels),
-                       calcDPCTable(skillLevels,paste(obsLevels),
-                                   alphas,betas,rules))
-  if (is.numeric(obsLevels) ||
-      names(result)[length(skillLevels)+1]!=paste(obsLevels[1])) {
-    ## R is "helpfully" fixing our numeric labels.  Need to insist.
-    names(result) <- c(names(skillLevels),paste(obsLevels))
-  }
-  result
 
+gradedResponse <- function (et,linkScale=NULL,obsLevels=NULL) {
+  k <- nrow(et)+1
+  pt <- 1/(1+exp(-1.7*et))
+  ## The cummax corrects for problems where the
+  ## Pr(X> k+1) is greater than Pr(X>k)
+  pt <- cbind(0,t(apply(pt,1,cummax)),1)
+  probs <- pt[,2:(k+1)]-pt[,1:k]
+  # probs <- ifelse(probs<0,0,probs)
+  if (!is.null(obsLevels)) {
+    dimnames(probs) <- list(NULL,obsLevels)
+  }
+  probs
 }
+
+
