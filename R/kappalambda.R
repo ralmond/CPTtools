922e63b15c5ec37e2fd9f619506c0e1d9d901229
Russell G Almond <russell.g.almond@gmail.com>
2021-07-01 15:38:20 -0400

    Added gamma.
    * R/kappalambda.R (gkGamma): Added gamma function to deal with
    non-square cases.

--- R/kappalambda.R
+++ R/kappalambda.R
@@ -37,3 +37,32 @@ fcKappa <- function (tab, weights=c("None","Linear","Quadratic"),
   expagree <-sum(W*outer(prow,pcol))
   (agree - expagree)/(1-expagree)
 }
+
+gkGamma <- function (tab) {
+    tab <- as.matrix(tab)
+    N <- sum(tab)
+    rtab <- tab/N
+    PIs <- PId <- 0
+    for (a in 1:(nrow(rtab)-1)) {
+        for (aa in (a+1):nrow(rtab)) {
+             for (b in 1:ncol(rtab)) {
+                 if (ncol(rtab) > b) {
+                     for (bb in (b+1):ncol(rtab)) {
+                         PIs <- PIs + rtab[a,b]*rtab[aa,bb]
+                     }
+                 }
+                 if (b > 1) {
+                     for (bb in 1:(b-1)) {
+                         PId <- PId + rtab[a,b]*rtab[aa,bb]
+                     }
+                 }
+             }
+        }
+    }
+    PIs <- 2*PIs
+    PId <- 2*PId
+    PIt <- 1-PIs-PId
+    (PIs-PId)/(1-PIt)
+    
+}
+
