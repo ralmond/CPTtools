498fcea5669108269242f115655a47ba279d6807
Russell G Almond <russell.g.almond@gmail.com>
2023-07-27 21:06:36 -0400

    Added tests for kappa, lambda and gamma.  Also added accuracy function.

--- R/kappalambda.R
+++ R/kappalambda.R
@@ -1,15 +1,31 @@
+accuracy <- function (tab, weights=c("None","Linear","Quadratic"),
+                      W=diag(nrow(tab))) {
+  if (!is.matrix(tab) && nrow(tab)==ncol(tab)) {
+    stop("Expected a square (confusion) matrix.")
+  }
+  if (missing(W)) {
+    W <- diffweights(nrow(tab),weights)
+  }
+  sum(W*tab)/sum(tab)
+}
 
+diffweights <- function(K, weights=c("None","Linear","Quadratic"),
+                        W=diag(K)) {
+  if (is.character(weights)) weights <- match.arg(weights)
+  switch(weights,
+         None=W,
+         Linear= 1 - abs(outer(1:K,1:K,"-"))/(K-1),
+         Quadratic= 1 - (outer(1:K,1:K,"-")/(K-1))^2
+  )
+}
+            
 gkLambda <- function (tab, weights=c("None","Linear","Quadratic"),
                       W = diag(nrow(tab))) {
   K <- nrow(tab)
   if (nrow(tab)!=ncol(tab))
     stop("Expected a square matrix.")
   if (missing(W)) {
-    if (is.character(weights)) weights <- match.arg(weights)
-    W <- switch(weights,
-                None=W,
-                Linear= W - abs(outer(1:K,1:K,"-"))/(K-1),
-                Quadratic= W - (outer(1:K,1:K,"-")/(K-1))^2)
+    W <- diffweights(nrow(tab),weights)
   }
   N <- sum(tab)
   prow <- rowSums(tab)/N
@@ -20,15 +36,10 @@ gkLambda <- function (tab, weights=c("None","Linear","Quadratic"),
 
 fcKappa <- function (tab, weights=c("None","Linear","Quadratic"),
                      W = diag(nrow(tab))) {
-  K <- nrow(tab)
   if (nrow(tab)!=ncol(tab))
     stop("Expected a square matrix.")
   if (missing(W)) {
-    if (is.character(weights)) weights <- match.arg(weights)
-    W <- switch(weights,
-                None=W,
-                Linear= W - abs(outer(1:K,1:K,"-"))/(K-1),
-                Quadratic= W - (outer(1:K,1:K,"-")/(K-1))^2)
+    W <- diffweights(nrow(tab),weights)
   }
   N <- sum(tab)
   agree <- sum(W*tab)/N
